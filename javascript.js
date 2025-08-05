77198e75c53825566f32d653aa1b3409327ad561
Justin Shaw <jusitnshawcareer@gmail.com>
2025-08-05 15:39:38 -0400

    added event slide to change grid size

--- javascript.js
+++ javascript.js
@@ -1,11 +1,37 @@
-const container = document.getElementById('container');
+const container = document.getElementById('grid-container');
+const gridSizeLabel = document.getElementById('grid-size-label');
+const gridSizeSlider = document.getElementById('grid-size-slider');
 const GRID_SIZE = 16;
 
-for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'black';
-    });
-    container.appendChild(square);
+// default grid
+createGrid(GRID_SIZE);
+
+// grid container
+function createGrid(grid_size) {
+    container.style.gridTemplateColumns = `repeat(${grid_size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${grid_size}, 1fr)`;
+    for (let i = 0; i < grid_size * grid_size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+        });
+        container.appendChild(square);
+    }
 }
+
+// grid slider
+gridSizeSlider.addEventListener('input', () => {
+    const newSize = gridSizeSlider.value;
+    gridSizeLabel.textContent = `${newSize} x ${newSize}`
+    container.innerHTML = '';
+    createGrid(newSize);
+
+})
+
+
+
+
+
+
+
